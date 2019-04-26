a74e5d1fb9c9ec7809c942a62189a9a093b87b0e
Michael Larragueta <m.larragueta@yahoo.com>
2019-04-26 08:52:21 -0700

    Add blank sheeets with test error message in readme

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WordCounter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
         }
     }
---
+++ WordCounter/WordCounter.Tests/ModelTests/WordTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class Word
+    {
+        // Test methods go here
+    }
+}
